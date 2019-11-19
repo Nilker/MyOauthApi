ec1d22eb69324848d56a2e2a0ff1fec92281b423
李宏利 <lihongli@eastfair.com>
2019-11-19 10:18:49 +0800

    1

--- MyOauthApi/OAuth2/ApplicationOAuthProvider.cs
+++ MyOauthApi/OAuth2/ApplicationOAuthProvider.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 
 /*----------------------------------------------------------------
@@ -15,7 +14,46 @@ using Microsoft.Owin.Security.OAuth;
 ----------------------------------------------------------------*/
 namespace MyOauthApi.OAuth2
 {
-    public class ApplicationOAuthProvider: OAuthAuthorizationServerProvider
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
     {
+        /// <summary>
+        ///     验证客户[client_id与client_secret验证]
+        /// </summary>
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //http://localhost:48339/token
+            string client_id;
+            string client_secret;
+            context.TryGetFormCredentials(out client_id, out client_secret);
+            if (client_id == "zara" && client_secret == "123456")
+                context.Validated(client_id);
+            else
+                //context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.OK);
+                context.SetError("invalid_client", "client is not valid");
+            return base.ValidateClientAuthentication(context);
+        }
+
+        /// <summary>
+        ///     客户端授权[生成access token]
+        /// </summary>
+        public override Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            var oAuthIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+            oAuthIdentity.AddClaim(new Claim(ClaimTypes.Name, "iphone"));
+            var ticket = new AuthenticationTicket(oAuthIdentity, new AuthenticationProperties {AllowRefresh = true});
+            context.Validated(ticket);
+            return base.GrantClientCredentials(context);
+        }
+
+        /// <summary>
+        ///     刷新Token[刷新refresh_token]
+        /// </summary>
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            //enforce client binding of refresh token
+            if (context.Ticket == null || context.Ticket.Identity == null || !context.Ticket.Identity.IsAuthenticated)
+                context.SetError("invalid_grant", "Refresh token is not valid");
+            return base.GrantRefreshToken(context);
+        }
     }
 }
